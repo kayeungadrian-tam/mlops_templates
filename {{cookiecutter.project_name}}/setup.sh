99fee42e7f5adabccb589e110aa0770c86a5ca40
adrian <tamkayeung.adrian@gmail.com>
2022-10-01 14:24:41 +0900

    fix type

--- {{cookiecutter.project_name}}/setup.sh
+++ {{cookiecutter.project_name}}/setup.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
+# Git init
+git init
+
 # Pre-commit
 if ! command -v pre-commit --version &>/dev/null; then
     pip install pre-commit
 fi
 pre-commit install
+pre-commit run --all-files
 
 # DVC
 if ! command -v dvc --version &>/dev/null; then
--- {{cookiecutter.project_name}}/src/featurize.py
+++ {{cookiecutter.project_name}}/src/featurize.py
@@ -7,10 +7,8 @@ import pickle
 
 # read command line params
 if len(sys.argv) != 3:
-    sys.stderr.write('Arguments error. Usage:\n')
-    sys.stderr.write(
-        '\tpython featurize.py data-dir-path features-dir-path\n'
-    )
+    sys.stderr.write("Arguments error. Usage:\n")
+    sys.stderr.write("\tpython featurize.py data-dir-path features-dir-path\n")
     sys.exit(1)
 
 data_path = sys.argv[1]
@@ -18,12 +16,13 @@ features_path = sys.argv[2]
 
 os.makedirs(features_path, exist_ok=True)
 
-train_input_file = os.path.join(data_path, 'train.csv')
-test_input_file  = os.path.join(data_path, 'test.csv')
+train_input_file = os.path.join(data_path, "train.csv")
+test_input_file = os.path.join(data_path, "test.csv")
 
 # read the data from file
 df_train = pd.read_csv(train_input_file)
 df_test = pd.read_csv(test_input_file)
+
 
 def extract_column(column, df_path):
     df = get_df(df_path)
@@ -31,18 +30,20 @@ def extract_column(column, df_path):
 
     return corpus
 
+
 def get_train_and_test_corpus(df_1, df_2):
     corpus_train = df_1["text"]
     corpus_test = df_2["text"]
 
-    return corpus_train.append(corpus_test) 
+    return corpus_train.append(corpus_test)
+
 
 def append_labels_and_save_pkl(df, tfidf_matrix, filename):
     output_file = os.path.join(features_path, filename)
     target = df[["target"]]
     output = pd.concat([pd.DataFrame(tfidf_matrix.toarray()), target], axis=1)
 
-    with open(output_file, 'wb') as f:
+    with open(output_file, "wb") as f:
         pickle.dump(output, f)
 
 
@@ -56,5 +57,5 @@ train_matrix = vectorizer.transform(df_train["text"])
 test_matrix = vectorizer.transform(df_test["text"])
 
 # save data to pickle (appending the labels column)
-append_labels_and_save_pkl(df_train, train_matrix, 'train.pkl')
-append_labels_and_save_pkl(df_test, test_matrix, 'test.pkl')
+append_labels_and_save_pkl(df_train, train_matrix, "train.pkl")
+append_labels_and_save_pkl(df_test, test_matrix, "test.pkl")
